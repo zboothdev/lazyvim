b33407d7839817ffea5c5311172d64fa9805424d
Zachary Booth <zbooth.dev@gmail.com>
2023-06-28 17:26:31 -0500

    Reset to default leader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Reset back to default leader.
+vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
