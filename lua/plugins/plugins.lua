3767fe84850d52a3cb499622460300bb41118c2a
Zachary Booth <zbooth.dev@gmail.com>
2023-06-29 11:24:37 -0500

    vim-tmux-navigator: Configure keymaps
    Configure the keymaps.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,13 @@
 return {
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      -- stylua: ignore
+      { "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", desc = "Tmux Nav Left", remap = false },
+      { "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", desc = "Tmux Nav Down", remap = false },
+      { "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", desc = "Tmux Nav Up", remap = false },
+      { "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", desc = "Tmux Nav Right", remap = false },
+    },
+  },
   { "jlanzarotta/bufexplorer" },
 }
