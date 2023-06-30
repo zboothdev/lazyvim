f37961d8477a0485574f8bd803fb7f33880f77d4
Zachary Booth <zbooth.dev@gmail.com>
2023-06-29 20:56:36 -0500

    neo-tree: Add custom keymap
    Add a keymap for <leader>o.

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      -- stylua: ignore
+      { "<leader>o", "<Cmd>Neotree reveal current<CR>", desc = "Reveal file in current buffer" },
+    },
+  },
+}
