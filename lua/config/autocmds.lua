2b871c2c8aec0a69fa2d6fb28af14efa5b0749d0
Zachary Booth <zbooth.dev@gmail.com>
2023-07-07 15:40:13 -0500

    Add options from vimrc
    Add options from vimrc.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[
+    augroup filetypefix
+    autocmd!
+        " Treat WAF wscript as a python file
+        autocmd BufNewFile,BufRead wscript* set filetype=python
+
+        " Udev rules
+        autocmd FileType hog set filetype=udevrules
+    augroup END
+]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,20 @@
 -- Reset back to default leader.
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
+
+vim.opt.relativenumber = false
+
+-- wildmode: A list of completions will be shown and the command
+-- will be completed to the longest common command.
+vim.opt.wildmode = "list:longest"
+
+-- Add options from vimrc
+vim.opt.showmatch = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.spell = true
+vim.opt.spelllang = "en_us"
+
+vim.opt.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<"
