822b46f3265367f89b217a8979f67b2007bcc487
Zachary Booth <zbooth.dev@gmail.com>
2023-08-10 10:12:12 -0500

    Enable extras.editor.mini-files
    Also update lock file.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
     -- import/override with your plugins
     { import = "plugins" },
   },
